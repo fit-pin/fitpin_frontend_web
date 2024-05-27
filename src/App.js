53b132c19db797b2e44dbbf72d6ece9a1ddede2a
01390i <li1793805@gmail.com>
2024-05-27 12:45:16 +0900

    mainpage

--- src/App.js
+++ src/App.js
@@ -199,55 +199,130 @@ function App() {
 
   return (
     <div ref={outerDivRef} className="outer">
-      <div className="inner">
-        <div className="menubar">
+      
 
-          <div className="menubarleft">
-          <p><span class="italianno">Fit</span>fin</p>
+
+      <div class="inner">
+        <div className="menu-bar">
+          <div className="left-section">
+            <span className="logo">Fit</span>fin
           </div>
-
-          <div className="menubarRight">
-            <p>핏 코멘트</p>
-            <p>고객 센터</p>
-            <p>수선 업체 로그인</p>
+          <div className="right-section">
+            <span className="menu-item">핏 코멘트</span>
+            <span className="menu-item">고객센터</span>
+            <span className="menu-item">수선 업체 로그인</span>
           </div>
-
         </div>
-        <div className="background">
-          <h1>내가 원하는 옷을</h1>
-          <h1>나만을 위한 수선과 함께</h1>
-          <span>Find Your</span>
-          <span>Fit fin</span>
-        </div>
-
       </div>
+
       <div className="divider"></div>
+
+
+
+      <div className="inner bg-black container">
+        <div className="left-section">
+          <img src="finger1.png" alt="finger1" className="leftimage" />
+        </div>
+        <div className="right-section">
+          <span className="white">내가 사고 싶은 제품을 고르고</span>
+        </div>
+      </div>
+
+      <div className="divider"></div>
+
+
+
+      <div className="inner bg-white container">
+        <div className="left-section">
+          <img src="cloth1.png" alt="cloth1" className="leftimage" />
+        </div>
+        <div className="right-section">
+          <span>원하는 사이즈로 커스텀하면</span>
+        </div>
+      </div>
+
+      <div className="divider"></div>
+
+
 
       <div className="inner bg-black">
-        2
-        <img src="./image/finger1.png"></img>
+        <div className="left-section">
+          <img src="box1.png" alt="box1" className="leftimage" />
+        </div>
+        <div className="right-section">
+          <span className="white">문 앞으로 배송해드려요</span>
+        </div>
       </div>
+
       <div className="divider"></div>
 
-      <div className="inner bg-white">3</div>
+
+
+      <div className="inner bg-white">
+        <div className="left-section">
+          <span>클릭 한 번이면 수선이 뚝딱</span>
+          <span>내가 원하는 제품을 원하는 핏으로 수선할 수 있어요</span>
+          <span>사고 싶은 제품을 선택한 뒤 내가 원하는 핏으로 사이즈를 조절해요</span>
+          <span>번거롭게 수선집에 가지 않아도 클릭 한 번이면 수선된 옷이 집 앞으로 배송돼요</span>
+        </div>
+        <div className="right-section">
+          <img src="iPhone1.png" alt="iPhone1" className="leftimage" />
+        </div>
+      </div>
+
       <div className="divider"></div>
 
-      <div className="inner bg-black">4</div>
+
+
+      <div className="inner bg-white">
+        <div className="left-section">
+          <img src="iPhone2.png" alt="iPhone2" className="leftimage" />
+        </div>
+        <div className="right-section">
+          <span>메인 페이지</span>
+          <span>내 체형, 취향을 모두 만족시킬</span>
+          <span>옷을 핏핀이 추천해드려요</span>
+        </div>
+      </div>
+
       <div className="divider"></div>
 
-      <div className="inner bg-white">5</div>
+
+
+      <div className="inner bg-white">
+        <div className="left-section">
+          <span>사이즈 정보 페이지</span>
+          <span>궁금한 제품의 사진을 찍으면</span>
+          <span>핏핀이 사이즈 정보를 알려드려요</span>
+        </div>
+        <div className="right-section">
+          <img src="iPhone3.png" alt="iPhone3" className="leftimage" />
+        </div>
+      </div>
+
       <div className="divider"></div>
 
-      <div className="inner bg-black">6</div>
+
+
+      <div className="inner bg-white">
+        <div className="left-section">
+          <img src="iPhone4.png" alt="iPhone4" className="leftimage" />
+          <img src="iPhone5.png" alt="iPhone5" className="leftimage" />
+        </div>
+        <div className="right-section">
+          <span>핏 코멘트 페이지</span>
+          <span>다른 사람들의 실착 후기가 궁금하다면?</span>
+          <span>핏 코멘트에서 확인해요</span>
+        </div>
+      </div>
+
       <div className="divider"></div>
 
-      <div className="inner bg-white">7</div>
-      <div className="divider"></div>
 
-      <div className="inner bg-black">8</div>
-      <div className="divider"></div>
 
-      <div className="inner bg-white">9</div>
+      <div className="inner bg-white">
+        9
+      </div>
     </div>
   );
 }
