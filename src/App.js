6f93e90516ca545db6d64b08f10d0543ec4e165f
Lseoksee <da864268@naver.com>
2024-06-22 16:28:11 +0900

    prettier 적용

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Route, Routes, BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Service from '../src/pages/Service';
 import Ask from '../src/pages/Ask';
 import Fitcomment from '../src/pages/Fitcomment';
@@ -13,24 +13,24 @@ import AuctionDetail from './pages/AuctionDetail';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
-        	<Routes>
-        	  <Route path="/" element={<Main />} />
-        	  <Route path="/Service" element={<Service />} />
-        	  <Route path="/Ask" element={<Ask />} />
-        	  <Route path="/Fitcomment" element={<Fitcomment />} />
-        	  <Route path="/Fitcomment2" element={<Fitcomment2 />} />
-        	  <Route path="/Login" element={<Login />} />
-        	  <Route path="/SignIn" element={<SignIn />} />
-        	  <Route path="/Repair" element={<Repair />} />
-        	  <Route path="/Auction" element={<Auction />} />
-        	  <Route path="/AuctionDetail" element={<AuctionDetail />} />
-        	</Routes>
-    	</BrowserRouter>
-      </div>
-  );
+	return (
+		<div className="App">
+			<BrowserRouter basename={process.env.PUBLIC_URL}>
+				<Routes>
+					<Route path="/" element={<Main />} />
+					<Route path="/Service" element={<Service />} />
+					<Route path="/Ask" element={<Ask />} />
+					<Route path="/Fitcomment" element={<Fitcomment />} />
+					<Route path="/Fitcomment2" element={<Fitcomment2 />} />
+					<Route path="/Login" element={<Login />} />
+					<Route path="/SignIn" element={<SignIn />} />
+					<Route path="/Repair" element={<Repair />} />
+					<Route path="/Auction" element={<Auction />} />
+					<Route path="/AuctionDetail" element={<AuctionDetail />} />
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
