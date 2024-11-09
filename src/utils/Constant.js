57873a3acb4c844fbbb9f62edac100bee281f67d
Lseoksee <da864268@naver.com>
2024-11-09 13:57:12 +0900

    DATA_URL_APP 주소 변경

--- src/utils/Constant.js
+++ src/utils/Constant.js
@@ -1,11 +1,17 @@
-//WEB 임시 API 주소
-// export const DATA_URL = 'http://localhost:8080/';
-
 //WEB 배포 API 주소
 export const DATA_URL = 'https://fitpin-web-back.kro.kr/';
 
 //APP API 주소
-export const DATA_URL_APP = 'http://fitpitback.kro.kr:8080/';
+export const DATA_URL_APP = 'https://fitpin-web-back.kro.kr/appserver/';
 
 // 웹소켓 주소
 export const WEB_SCOKET_URL = "wss://fitpin-web-back.kro.kr/sock";
+
+
+/** 테스트용 주소 */
+
+/* //APP API 주소
+export const DATA_URL_APP = 'https://fitpin-web-back.kro.kr/appserver/';
+
+// 웹소켓 주소
+export const WEB_SCOKET_URL = "wss://fitpin-web-back.kro.kr/sock"; */
