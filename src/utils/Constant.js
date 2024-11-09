b1fe950431c99713469948d977afaee90745ead0
Lseoksee <da864268@naver.com>
2024-11-09 14:41:53 +0900

    주소 커밋 추가

--- src/utils/Constant.js
+++ src/utils/Constant.js
@@ -1,8 +1,8 @@
-//WEB 배포 API 주소
-export const DATA_URL = 'https://fitpin-web-back.kro.kr/';
-
 //APP API 주소
 export const DATA_URL_APP = 'https://fitpin-web-back.kro.kr/appserver/';
+
+//WEB 배포 API 주소
+export const DATA_URL = 'https://fitpin-web-back.kro.kr/';
 
 // 웹소켓 주소
 export const WEB_SCOKET_URL = "wss://fitpin-web-back.kro.kr/sock";
@@ -10,8 +10,8 @@ export const WEB_SCOKET_URL = "wss://fitpin-web-back.kro.kr/sock";
 
 /** 테스트용 주소 */
 
-/* //APP API 주소
-export const DATA_URL_APP = 'https://fitpin-web-back.kro.kr/appserver/';
+/* //WEB API 주소
+export const DATA_URL = 'http://localhost:8080/';
 
 // 웹소켓 주소
-export const WEB_SCOKET_URL = "wss://fitpin-web-back.kro.kr/sock"; */
+export const WEB_SCOKET_URL = "ws://localhost:8080/sock"; */
