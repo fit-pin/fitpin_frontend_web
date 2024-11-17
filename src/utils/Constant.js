8ef44dd855b3e924efa64a13e2ab566d838c1807
Lseoksee <da864268@naver.com>
2024-11-17 17:21:09 +0900

    주소및 readme 수정

--- src/utils/Constant.js
+++ src/utils/Constant.js
@@ -1,11 +1,11 @@
 //APP API 주소
-export const DATA_URL_APP = 'https://fitpin-web-back.kro.kr/appserver/';
+export const DATA_URL_APP = 'https://fitpin.kro.kr/app-api/';
 
 //WEB 배포 API 주소
-export const DATA_URL = 'https://fitpin-web-back.kro.kr/';
+export const DATA_URL = 'https://fitpin.kro.kr/web-api/';
 
 // 웹소켓 주소
-export const WEB_SCOKET_URL = "wss://fitpin-web-back.kro.kr/sock";
+export const WEB_SCOKET_URL = "wss://fitpin.kro.kr/web-api/sock";
 
 
 /** 테스트용 주소 */
